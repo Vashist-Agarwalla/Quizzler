a8339326b58f13a6239c53e7a036eb473026ee23
Vashist-Agarwalla <vashist1202@gmail.com>
2022-01-02 23:15:20 +0530

    ⚡️ Use Encapsulation

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
-  int questionNumber = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -44,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                quizBrain.questionBank[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -71,13 +69,13 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                if (quizBrain.questionBank[questionNumber].questionAnswer == true)
+                if (quizBrain.getQuestionAnswer() == true)
                   print('correct');
                 else
                   print('wrong');
 
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
               },
             ),
@@ -99,12 +97,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                if (quizBrain.questionBank[questionNumber].questionAnswer == false)
+                if (quizBrain.getQuestionAnswer() == false)
                   print('correct');
                 else
                   print('wrong');
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
               },
             ),
